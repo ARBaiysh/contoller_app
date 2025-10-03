7c620dc63d0d8490a9a3e00ed735e43956ac894d
ARBaiysh <o0052i@mail.ru>
2025-10-04 01:34:10 +0600

    init 2

--- lib/app/modules/subscriber_detail/widgets/subscriber_info_card.dart
+++ lib/app/modules/subscriber_detail/widgets/subscriber_info_card.dart
@@ -1,5 +1,3 @@
-// lib/app/modules/subscriber_detail/widgets/subscriber_info_card.dart
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:url_launcher/url_launcher.dart';
